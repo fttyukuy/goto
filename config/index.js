19bb85b135d769c1a7a61892aef1ca6a11167aea
fttyukuy <47567266+fttyukuy@users.noreply.github.com>
2019-08-13 20:23:16 +0800

    修改打包路径

--- config/index.js
+++ config/index.js
@@ -56,8 +56,8 @@ module.exports = {
 
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
-    assetsSubDirectory: 'goto/dist/static',
-    assetsPublicPath: '/',
+    assetsSubDirectory: 'static',
+    assetsPublicPath: '/goto/dist/',
 
     /**
      * Source Maps
